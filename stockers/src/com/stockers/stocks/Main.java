f5ae1b7a8e9af4c2462a2946bcd55dec17d8f61f
chandan <chandan.krc361991@gmail.com>
2021-09-18 14:41:11 +0530

    Stocker Project

---
+++ stockers/src/com/stockers/stocks/Main.java
@@ -0,0 +1,61 @@
+package com.stockers.stocks;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice;
+		Scanner in = new Scanner(System.in);
+		StockExchange stock = new StockExchange();
+		stock.initialSetup();
+
+		do {
+			System.out.println(" -----------------------------------------------\n"
+					+ "Enter the operation that you want to perform\n"
+					+ "1. Display the companies stock prices in ascending order\n"
+					+ "2. Display the companies stock prices in descending order\n"
+					+ "3. Display the total no of companies for which stock prices rose today\n"
+					+ "4. Display the total no of companies for which stock prices declined today\n"
+					+ "5. Search a specific stock price\n" + "6. press 0 to exit\n"
+					+ "-----------------------------------------------\n");
+			System.out.println();
+
+			choice = in.nextInt();
+			switch (choice) {
+			case 0:
+				choice = 0;
+				break;
+			case 1: {
+				System.out.println("Stock prices in ascending order are : ");
+				stock.ascendingOrder(stock.shareprice, 0, stock.shareprice.length - 1);
+				stock.printValues();
+			}
+				break;
+			case 2: {
+				System.out.println("Stock prices in descending order are : ");
+				stock.ascendingOrder(stock.shareprice, 0, stock.shareprice.length - 1);
+				stock.descendingOrder();
+			}
+				break;
+			case 3: {
+				System.out.println("Total no of companies whose stock price rose today : " + stock.bullish);
+			}
+				break;
+			case 4: {
+				System.out.println("Total no of companies whose stock price declined today : " + stock.bearish);
+			}
+				break;
+			case 5: {
+				stock.searchValue();
+			}
+				break;
+			default:
+				System.out.println("Invalid choice. Enter a valid choice.\n");
+			}
+		} while (choice != 0);
+		System.out.println("Exited Successfully!!!");
+		in.close();
+	}
+}
---
+++ stockers/src/com/stockers/stocks/StockExchange.java
@@ -0,0 +1,108 @@
+package com.stockers.stocks;
+
+import java.util.Scanner;
+
+class StockExchange {
+
+	public int noOfCompanies;
+	public double[] shareprice;
+	boolean priceRise;
+	int bullish = 0, bearish = 0, c = 0;
+
+	Scanner sc = new Scanner(System.in);
+
+	public void initialSetup() {
+		System.out.println("Enter the no of companies: ");
+		noOfCompanies = sc.nextInt();
+		shareprice = new double[noOfCompanies];
+		insertValues();
+	}
+
+	public void insertValues() {
+		for (int i = 0; i < noOfCompanies; i++) {
+			c = i + 1;
+			System.out.print("Enter current stock price of the company " + c + ": ");
+			shareprice[i] = sc.nextDouble();
+			System.out.println("Whether company's stock price rose today compare to yesterday?");
+			priceRise = sc.nextBoolean();
+			if (priceRise == true) {
+				bullish++;
+			} else {
+				bearish++;
+			}
+		}
+	}
+
+	public void ascendingOrder(double array[], int l, int r) {
+		if (l < r) {
+			int m = l + (r - l) / 2;
+			ascendingOrder(array, l, m);
+			ascendingOrder(array, m + 1, r);
+			conquerArray(array, l, m, r);
+		}
+	}
+
+	public void conquerArray(double array[], int l, int m, int r) {
+		int n1 = m - l + 1;
+		int n2 = r - m;
+		double L[] = new double[n1];
+		double R[] = new double[n2];
+		for (int i = 0; i < n1; ++i) {
+			L[i] = array[l + i];
+		}
+		for (int j = 0; j < n2; ++j) {
+			R[j] = array[m + 1 + j];
+		}
+		int i = 0, j = 0;
+		int k = l;
+		while (i < n1 && j < n2) {
+			if (L[i] <= R[j]) {
+				array[k] = L[i];
+				i++;
+			} else {
+				array[k] = R[j];
+				j++;
+			}
+			k++;
+		}
+		while (i < n1) {
+			array[k] = L[i];
+			i++;
+			k++;
+		}
+		while (j < n2) {
+			array[k] = R[j];
+			j++;
+			k++;
+		}
+	}
+
+	public void printValues() {
+		for (int i = 0; i < shareprice.length; i++) {
+			System.out.print(shareprice[i] + "  ");
+		}
+		System.out.println();
+	}
+
+	public void descendingOrder() {
+		for (int i = shareprice.length - 1; i >= 0; i--) {
+			System.out.print(shareprice[i] + "  ");
+		}
+		System.out.println();
+	}
+
+	public void searchValue() {
+		boolean status = false;
+		System.out.println("Enter the stock price to search : ");
+		double value = sc.nextDouble();
+		for (int i = 0; i < shareprice.length; i++) {
+			if (shareprice[i] == value) {
+				System.out.println("Stock of value " + value + " is present");
+				status = true;
+			}
+		}
+		if (status == false) {
+			System.out.println("Stock Value not found!!!");
+		}
+	}
+}
